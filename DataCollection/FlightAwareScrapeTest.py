b2b0efbc0618323f9ca7804f399b551eeb5f4db1
Sam Guleff <sam.guleff@jet.com>
2016-09-25 23:02:44 -0400

    Added scrape testing code

---
+++ DataCollection/FlightAwareScrapeTest.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import urllib
+
+url = 'https://flightaware.com/live/flight/UAL88/history/20160924/0745Z/ZBAA/KEWR/tracklog'
+
+r = urllib.urlopen(url).read()
+soup = BeautifulSoup(r)
+
+table = soup.find("table", {"id":"tracklogTable"})
+
+table_body = table.find('tbody')
+
+data = []
+
+rows = table.find_all('tr')
+
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    if len(cols) == 10:
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+
